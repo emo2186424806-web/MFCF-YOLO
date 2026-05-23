4042590c82f1c0fb9a08fbacd39275da063a704b
UltralyticsAssistant <web@ultralytics.com>
2026-05-23 18:04:35 +0000

    Auto-format by https://ultralytics.com/actions

--- 训练脚本.py
+++ 训练脚本.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     model.train(
         data=r"coco8.yaml",
-        epochs=10,                                 #10轮
+        epochs=10,  # 10轮
         batch=1,
         imgsz=640,
         workers=0,
